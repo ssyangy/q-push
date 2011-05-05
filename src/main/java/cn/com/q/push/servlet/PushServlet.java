7efd8fb54c280db2f036d1e374014469a53fb9f6
seanlinwang <xalinx@gmail.com>
2011-05-05 10:45:09 +0800

    refactor

--- src/main/java/cn/com/q/push/servlet/PushServlet.java
+++ src/main/java/cn/com/q/push/servlet/PushServlet.java
@@ -4,15 +4,10 @@
 package cn.com.q.push.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.AsyncContext;
@@ -29,7 +24,6 @@ import org.apache.log4j.Logger;
 import org.springframework.util.CollectionUtils;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPubSub;
 
 /**
  * @author seanlinwang at gmail dot com
@@ -45,91 +39,19 @@ public class PushServlet extends HttpServlet {
 
 	private static final int MIN_ALIVE_TIME = 30 * 1000;// min alive minute
 
-	private static class PushExecutor implements Runnable {
+	private Jedis subJedis = null;
 
-		private boolean closed = false;
-
-		private AsyncContext ctx;
-
-		private static final int MAX_LINE_POOL_SIZE = 1024 * 10;
-
-		private static final int POLL_TIMEOUT = 1000;
-
-		private BlockingQueue<Object> msgPool = new ArrayBlockingQueue<Object>(MAX_LINE_POOL_SIZE);
-
-		private final Object eof = new Object();
-
-		public void putMsg(Object msg) throws InterruptedException {
-			msgPool.put(msg);
-		}
-
-		public boolean isEnded() {
-			return this.closed == true;
-		}
-
-		public void end() throws InterruptedException {
-			putMsg(eof);
-		}
-
-		public PushExecutor(AsyncContext ctx) {
-			this.ctx = ctx;
-		}
-
-		public void run() {
-			long startTime = System.currentTimeMillis();
-			PrintWriter out = null;
-			try {
-				out = ctx.getResponse().getWriter();
-				Object msg = null;
-				while (true) {
-					if (System.currentTimeMillis() - startTime > MAX_ALIVE_TIME) {
-						break;
-					}
-					try {
-						msg = (Object) msgPool.poll(POLL_TIMEOUT, TimeUnit.MILLISECONDS);
-					} catch (InterruptedException e) {
-						log.error("", e);
-						continue;
-					}
-					if (msg == eof) {
-						break;
-					}
-					if (msg != null) {
-						log.debug("write:" + msg);
-						out.write((String) msg);
-						out.flush();
-					}
-				}
-			} catch (Exception e) {
-				log.debug("out closed", e);
-			} finally {
-				try {
-					out.close();
-				} catch (Exception e) {
-				}
-				try {
-					ctx.complete();
-				} catch (Exception e) {
-				}
-			}
-			this.closed = true;
-			this.msgPool = null;
-		}
-	}
-
-	private Jedis jedis = null;
+	private Jedis cacheJedis = null;
 
 	@Override
 	public void init() throws ServletException {
 		ServletConfig config = this.getServletConfig();
-		String cacheHost = config.getInitParameter("cacheHost");
-		int cachePort = Integer.valueOf(config.getInitParameter("cachePort"));
-		int cacheTimeout = Integer.valueOf(config.getInitParameter("cacheTimeout"));
-		jedis = new Jedis(cacheHost, cachePort, cacheTimeout);
+		String subHost = config.getInitParameter("subHost");
+		int subPort = Integer.valueOf(config.getInitParameter("subPort"));
+		int subTimeout = Integer.valueOf(config.getInitParameter("subTimeout"));
+		subJedis = new Jedis(subHost, subPort, subTimeout);
 		try {
-			jedis.connect();
-		} catch (UnknownHostException e) {
-			throw new ServletException(e);
+			subJedis.connect();
 		} catch (IOException e) {
 			throw new ServletException(e);
 		}
@@ -141,49 +63,38 @@ public class PushServlet extends HttpServlet {
 				while (true) {
 					try {
 						int tryTime = tryTimes.incrementAndGet();
-						if (tryTime == 5) {
-							log.error("init redis fail, try times:" + tryTime);
-							break;
+						if (tryTime != 1) {
+							log.error("subscribe redis fail, try times:" + tryTime);
+							if (tryTime == 4) {
+								break;
+							}
 						}
-						jedis.psubscribe(new JedisPubSub() {
 
-							@Override
-							public void onMessage(String channel, String message) {
+						subJedis.subscribe(new JedisPubSubWraper() { // subscribe message and push message content to http clients
 
-							}
+									@Override
+									public void onMessage(String channel, String message) {
+										try {
+											// message[type:peopleId,content]
+											String[] items = StringUtils.split(message, ":", 3);
+											// String type = items[0];
+											if ("weibo".equals(channel)) { // 1 identified weibo
+												String weiboSenderId = items[1];
+												// String weiboContent = items[2];
+												pushWeibo(weiboSenderId);
+											} else if ("reply".equals(channel)) {
+												String quoteSenderId = items[1];
+												// String replyContent = items[2];
+												pushWeiboReply(quoteSenderId);
+											}
+										} catch (Exception e) {
+											log.error("onMessage, message:" + message + ",channel:" + channel, e);
+										}
+									}
 
-							@Override
-							public void onPMessage(String pattern, String channel, String message) {
-								try {
-									String[] items = StringUtils.split(message, ":", 2);
-									pushMsg(items[0], items[1]);
-								} catch (Exception e) {
-									log.error("message invalid:" + message + " channel:" + channel, e);
-								}
-							}
-
-							@Override
-							public void onSubscribe(String channel, int subscribedChannels) {
-
-							}
-
-							@Override
-							public void onUnsubscribe(String channel, int subscribedChannels) {
-
-							}
-
-							@Override
-							public void onPUnsubscribe(String pattern, int subscribedChannels) {
-								log.error("onPUnsubscribe:" + pattern);
-							}
-
-							@Override
-							public void onPSubscribe(String pattern, int subscribedChannels) {
-
-							}
-						}, "msg-*");
+								}, "weibo", "reply", "message", "at");
 					} catch (Exception e) {
-						log.error("", e);
+						log.error("subscribe fail", e);
 					}
 					try {
 						Thread.sleep(500);
@@ -193,6 +104,46 @@ public class PushServlet extends HttpServlet {
 				}
 			}
 		}).start();
+
+		String cacheHost = config.getInitParameter("cacheHost");
+		int cachePort = Integer.valueOf(config.getInitParameter("cachePort"));
+		int cacheTimeout = Integer.valueOf(config.getInitParameter("cacheTimeout"));
+		this.cacheJedis = new Jedis(cacheHost, cachePort, cacheTimeout);
+	}
+
+	private void pushWeiboReply(String quoteSenderId) {
+		int repliedNumber = cacheJedis.incr("reply " + quoteSenderId);
+		push("mine", quoteSenderId, "reply new " + repliedNumber + "\n", false);
+
+	}
+
+	private void pushWeibo(String senderId) {
+		push("weibo", senderId, "weibo new\n", true);
+	}
+
+	private void push(String cmd, String peopleId, String msg, boolean closeAfterPush) {
+		List<PushExecutor> list = executorMap.get(peopleId);
+		if (CollectionUtils.isEmpty(list)) {
+			return;
+		}
+		synchronized (list) {
+			for (Iterator<PushExecutor> iter = list.iterator(); iter.hasNext();) {
+				PushExecutor exe = iter.next();
+				if (exe.isEnded()) {
+					iter.remove();
+					if (log.isDebugEnabled()) {
+						log.debug("remove executor:" + exe);
+					}
+					continue;
+				}
+				if (exe.getCmd().equals(cmd)) {
+					exe.putMsg(msg);
+					if (closeAfterPush) {
+						exe.end(); // ended immediately after push
+					}
+				}
+			}
+		}
 	}
 
 	private ConcurrentHashMap<String, List<PushExecutor>> executorMap = new ConcurrentHashMap<String, List<PushExecutor>>();
@@ -205,6 +156,7 @@ public class PushServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String[] peopleIds = StringUtils.split(req.getParameter("peopleIds"), ',');
+		String cmd = req.getParameter("cmd");
 		long aliveTime = 0;
 		String time = req.getParameter("aliveTime");
 		if (time == null) {
@@ -218,8 +170,10 @@ public class PushServlet extends HttpServlet {
 			}
 		}
 		resp.setContentType("application/json;charset=UTF-8");
-		AsyncContext ctx = req.startAsync(req, resp);
+		final AsyncContext ctx = req.startAsync(req, resp);
 		ctx.setTimeout(aliveTime);
+		final PushExecutor exe = new PushExecutor(ctx, cmd);
+		// exe.setAliveTime(aliveTime);
 		ctx.addListener(new AsyncListener() {
 
 			@Override
@@ -230,11 +184,13 @@ public class PushServlet extends HttpServlet {
 			@Override
 			public void onTimeout(AsyncEvent event) throws IOException {
 				log.debug("onTimeout");
+				exe.end();
 			}
 
 			@Override
 			public void onError(AsyncEvent event) throws IOException {
 				log.debug("onError");
+				exe.end();
 			}
 
 			@Override
@@ -242,41 +198,20 @@ public class PushServlet extends HttpServlet {
 				log.debug("onStartAsync");
 			}
 		});
-		PushExecutor exe = new PushExecutor(ctx);
-		log.debug("new executor");
-		addExecutor(peopleIds, exe);
+		subscribePeople(peopleIds, exe);
 		new Thread(exe).start();
-	}
-
-	private void pushMsg(String peopleId, String msg) {
-		List<PushExecutor> list = executorMap.get(peopleId);
-		if (CollectionUtils.isEmpty(list)) {
-			return;
-		}
-		synchronized (list) {
-			for (Iterator<PushExecutor> iter = list.iterator(); iter.hasNext();) {
-				PushExecutor exe = iter.next();
-				if (exe.isEnded()) {
-					iter.remove();
-					if (log.isDebugEnabled()) {
-						log.debug("remove exe, peopleId:" + peopleId);
-					}
-					continue;
-				}
-				try {
-					exe.putMsg(msg);
-				} catch (InterruptedException e) {
-					log.error("", e);
-				}
-			}
-		}
+		log.debug("start executor:" + exe);
 	}
 
 	/**
+	 * PushExecutor subscribe peoples' messages by peopleIds.
+	 * 
+	 * If message arrived, PushExecutor witch subscribed corresponding people's message, will be pushed.
+	 * 
 	 * @param peopleIds
-	 * @param exe
+	 * @param pushExecutor
 	 */
-	private void addExecutor(String[] peopleIds, PushExecutor exe) {
+	private void subscribePeople(String[] peopleIds, PushExecutor pushExecutor) {
 		for (String peopleId : peopleIds) {
 			List<PushExecutor> list = executorMap.get(peopleId);
 			if (list == null) {
@@ -288,7 +223,7 @@ public class PushServlet extends HttpServlet {
 				}
 			}
 			synchronized (list) {
-				list.add(exe);
+				list.add(pushExecutor);
 			}
 		}
 	}
